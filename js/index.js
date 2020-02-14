01aed05d0c77b83690ad956ac052462e0a9f356d
Abdelkrim Eljaouhari <a.eljaouhari@pyxicom.com>
2020-02-14 16:49:40 +0100

    3 pages completetd

--- js/index.js
+++ js/index.js
@@ -10,4 +10,40 @@
       scrolled = true;
     }
   });
+
+  let warning_msg = document.querySelector('.warning-msg');
+  let form_btn = document.querySelector('button');
+  let text_field = document.querySelectorAll('.text-field');
+  // set border to none
+  let borderToNone = () => {
+    [...text_field].map(item => (item.style.border = 'none'));
+  };
+  // callback haveToFill
+  let haveToFill = element => {
+    warning_msg.style.display = 'block';
+    warning_msg.textContent = `Vous devez remplir ${element}`;
+    setTimeout(() => {
+      warning_msg.style.display = 'none';
+      borderToNone();
+    }, 9000);
+  };
+  form_btn.addEventListener('click', e => {
+    e.preventDefault();
+    let mustBeFilled = [];
+    for (let i = 0; i < [...text_field].length; i++) {
+      if (![...text_field][i].value) {
+        mustBeFilled.push([...text_field][i].name);
+      }
+    }
+    if (mustBeFilled.length > 4) {
+      haveToFill('tous les champes');
+    } else if (mustBeFilled.length > 0) {
+      haveToFill(mustBeFilled.join(' , '));
+      [...text_field].map(item => {
+        if (mustBeFilled.includes(item.name)) {
+          item.style.border = '1px solid red';
+        }
+      });
+    }
+  });
 })();
